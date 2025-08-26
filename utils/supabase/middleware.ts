abf4dc23ee9fb4152bb53ce498392aabf1401797
Oladokun Oladipupo <65833243+oladokun-o@users.noreply.github.com>
2025-08-26 11:22:31 +0100

    Refactor Supabase middleware logic
    Moved Supabase session update logic to a new utility file for better separation of concerns. Updated the main middleware to use the new utility and improved matcher configuration for request paths.

---
+++ utils/supabase/middleware.ts
@@ -0,0 +1,29 @@
+import { createServerClient } from "@supabase/ssr";
+import { type NextRequest, NextResponse } from "next/server";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const updateSession = async (request: NextRequest) => {
+  let response = NextResponse.next({
+    request,
+  });
+
+  const supabase = createServerClient(supabaseUrl, supabaseKey, {
+    cookies: {
+      getAll() {
+        return request.cookies.getAll();
+      },
+      setAll(cookiesToSet) {
+        cookiesToSet.forEach(({ name, value, options }) => {
+          response.cookies.set(name, value, options);
+        });
+      },
+    },
+  });
+
+  // 👇 refresh session here
+  await supabase.auth.getUser();
+
+  return response;
+};
